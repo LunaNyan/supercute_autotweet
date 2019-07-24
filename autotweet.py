b0e2569eb3e8721d0eb2ca471d021f79c270b35f
lunanyan <chkong1998@gmail.com>
2019-07-25 01:02:43 +0900

    Initial commit

---
+++ autotweet.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3.7
+
+import tweepy, m_config
+from random import randint
+from time import sleep
+
+auth = tweepy.OAuthHandler(m_config.CONSUMER_KEY, m_config.CONSUMER_SECRET)
+auth.set_access_token(m_config.ACCESS_TOKEN, m_config.ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+bef = 0
+aft = 0
+
+while True:
+    while bef == aft:
+        aft = randint(0, len(m_config.array) - 1)
+    try:
+        api.update_status(m_config.array[aft])
+        print("bef : " + str(bef) + ", aft : " + str(aft))
+        bef = aft
+        delay = randint(20000, 30000)
+        print("delay : " + str(delay))
+        sleep(delay)
+    except:
+        continue
